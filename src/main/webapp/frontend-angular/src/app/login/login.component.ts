bb643b4f4d0d02d9de579bbda407e84c3efafc7d
ormorenoo <55900051+ormorenoo@users.noreply.github.com>
2024-09-10 19:32:25 -0500

    cambios front

--- src/main/webapp/frontend-angular/src/app/login/login.component.ts
+++ src/main/webapp/frontend-angular/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IAuthResponse, IUser } from '../models/user.model';
+import { GeneralService } from '../services/general.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username = '';
+  password = '';
 
-  constructor() { }
+  constructor(private generalService: GeneralService) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    const loginData: IUser = {
+      id: undefined,
+      login: this.username,
+      password: this.password
+    }
+    this.generalService.login(loginData).subscribe((res: HttpResponse<IAuthResponse>) => {
+      const auth = res.body;
+      if(auth) {
+        localStorage.setItem('jwtToken', auth.token); 
+      }
+      
+    });
   }
 
 }
